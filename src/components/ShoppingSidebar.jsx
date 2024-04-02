fc6e00c2616541eb29bb35e8683befe855c76824
Anthony McDonald <89093671+Anthony-McDonald@users.noreply.github.com>
2024-04-02 17:54:23 +0100

    demo purchase button added

--- src/components/ShoppingSidebar.jsx
+++ src/components/ShoppingSidebar.jsx
@@ -13,10 +13,19 @@ export default function ShoppingSidebar({ sidebarVisible, basketArray, removeFro
     setTotalPrice(totalPrice);
   }, [basketArray])
 
+
+  function purchaseButton() {
+    alert("This is a demo website, items cannot be purchased")
+  }
+
   return (
     <div className={`shopping-sidebar-container ${sidebarVisible ? "active" : ""}`}>
       <h1>Shopping Cart</h1>
+      <div className="price-and-buy">
       <h5>Total price: £{totalPrice}</h5>
+      <button onClick={purchaseButton} className="purchase-button">Purchase Items</button>
+      </div>
+
       <div className="basket-entry">
             {basketArray.map((product, index) => (
                 <BasketEntry key={index} index={index} product={product} removeFromBasket={removeFromBasket}/>
